e956b7000b9f2a4e3bf8a6426635dbd06adaa34c
Saksham Gupta <saksham757gupta@gmail.com>
2023-01-26 20:23:38 +0530

    60 getter and setter

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-def greet(fx):
-    def func(*args, **kwargs):
-        print("Good morning")
-        print(fx(*args, **kwargs))
-        print("Thank you for using this fxn")
-    return func
+class MyClass:
+    def __init__(self, value) -> None:
+        self._value = value
 
-@greet
-def add(a,b):
-    return a+b
+    def show(self):
+        print(self._value)
+    
+    @property
+    def ten_value(self):
+        return 10 * self._value
+    
+    @ten_value.setter
+    def ten_value(self, new_value):
+        self._value = new_value/10
 
-add(3,4) 
+
+a = MyClass(34)
+a._value = 67
+a.show()
