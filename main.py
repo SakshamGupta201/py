bba32f6c19a989ab809d226b568b4ce808bc8449
Saksham Gupta <saksham757gupta@gmail.com>
2023-01-26 11:59:09 +0530

    51

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-x: int = 10
-def func():
-    global x
-    x = 12.0
-func()
-print(type(x))
+with open("myfile.txt", 'r') as f:
+    while (True):
+        line = f.readline()
+        if not line:
+            break
+        marks = line.split(',')
+        m1 = marks[0]
+        m2 = marks[1]
+        m3 = marks[2].replace("\n",'')
+        print(f"Marks of student1 in Math {m1}")
+        print(f"Marks of student1 in Science {m2}")
+        print(f"Marks of student1 in SST {m3}")
